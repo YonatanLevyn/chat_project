01b7c5e5a793b582f5aff2592f817884c4fb97b3
YonatanLevyn <yonatanlevyn@gmail.com>
2023-03-29 15:40:22 +0300

    Updated .gitignore, settings.py, and requirements.txt

--- chat_project/settings.py
+++ chat_project/settings.py
@@ -86,10 +86,15 @@ WSGI_APPLICATION = 'chat_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'chat_db',
+        'USER': 'chat_user',
+        'PASSWORD': 'chat_password',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
